40e4bb9fdd74cbee614fdde38f4bfef6f28195af
Bicicletas <piter.aa22@gmail.com>
2023-06-13 12:12:36 +0200

    A

--- Assets/Scripts/Enemy/Enemy_IdleBehaviour.cs
+++ Assets/Scripts/Enemy/Enemy_IdleBehaviour.cs
@@ -28,10 +28,10 @@ public class Enemy_IdleBehaviour : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //
+    //}
 }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
